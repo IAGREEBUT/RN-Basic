11cdaa932b648973550f282b1eb0c7a43a188952
YujeongLEE <dldbwjd24@gmail.com>
2024-10-01 01:25:10 +0900

    feat : custom button (공통)

---
+++ src/components/CustomButton.tsx
@@ -0,0 +1,46 @@
+//react import
+import React, {useState} from 'react';
+import {Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+type BtnProps = {
+  btnColor: string;
+  btnTxtColor: string;
+  btnTxt: string;
+  disabled: boolean;
+};
+
+const CustomButton = (props: BtnProps) => {
+  return (
+    <>
+      <TouchableOpacity
+        style={[
+          styles.mainBtn,
+          {backgroundColor: props.disabled ? '#D9D9D9' : props.btnColor},
+        ]}
+        disabled={props.disabled}>
+        <Text style={[styles.mainBtnTxt, {color: props.btnTxtColor}]}>
+          {props.btnTxt}
+        </Text>
+      </TouchableOpacity>
+    </>
+  );
+};
+
+export default CustomButton;
+
+const styles = StyleSheet.create({
+  mainBtn: {
+    height: 48,
+    borderRadius: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  mainBtnTxt: {
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
