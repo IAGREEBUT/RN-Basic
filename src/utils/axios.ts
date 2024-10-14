a136ee887d00b5cc20c0bed04a7ad103eb9037da
YJLEE <dldbwjd24@gmail.com>
2024-10-14 16:56:11 +0900

    feat : axios setting

---
+++ src/utils/axios.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+//android's localhost : 10.0.2.2 
+const BASEURL = "http://10.0.2.2:5001/";
+
+const axiosInstance = axios.create({
+  baseURL: BASEURL,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    console.log(config.url)
+    return config
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    return Promise.reject(error);
+  },
+);
+
+export default axiosInstance;
