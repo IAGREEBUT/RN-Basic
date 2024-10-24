81fcf6032be734464403b276176285f9204fbc25
YJLEE <dldbwjd24@gmail.com>
2024-10-24 20:27:24 +0900

    feat : axios설정

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 //android's localhost : 10.0.2.2 
-const BASEURL = "http://10.0.2.2:5001/";
+const BASEURL = "http://10.0.2.2:3001/";
+
 
 const axiosInstance = axios.create({
   baseURL: BASEURL,
